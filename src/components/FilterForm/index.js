879c481a989b51a8825cf64ce1017d3cb6614787
Ezio C Morais <eziocm@gmail.com>
2020-02-13 09:54:05 -0300

    Datapicker settings

--- src/components/FilterForm/index.js
+++ src/components/FilterForm/index.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react';
-import DatePicker, { registerLocale } from "react-datepicker";
+import DatePicker, { registerLocale } from 'react-datepicker';
+import { subMonths, getDate, differenceInDays } from 'date-fns'
+import el from 'date-fns/locale/pt-BR';
 
-
-import "react-datepicker/dist/react-datepicker.css";
-import el from "date-fns/locale/pt-BR";
+import 'react-datepicker/dist/react-datepicker.css';
 
 registerLocale("el", el);
 
 function FilterForm() {
-  const [startDate, setStartDate] = useState();
-  const [endDate, setEndDate] = useState();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  
+  function renderDayContents(date) {
+    const tooltipText = `Tooltip for date: ${date}`;
+    return <span title={tooltipText}>{getDate(date)}</span>;
+  };
 
   return (
     <div>
@@ -19,16 +24,29 @@ function FilterForm() {
             <label htmlFor="data-ini">Data inicial:</label>
             <DatePicker
               locale="el" 
+              dateFormat="dd/MM/yyyy"
+              minDate={subMonths(new Date(), 12)}
+              maxDate={endDate}
               selected={startDate}
-              onChange={setStartDate}
+              onChange={date => setStartDate(date)}
+              todayButton="Hoje"
+              renderDayContents={date => renderDayContents(date)}
+              disabledKeyboardNavigation
             />
           </div>
           <div className="input-block">
             <label htmlFor="data-fin">Data final:</label>
             <DatePicker
               locale="el" 
+              dateFormat="dd/MM/yyyy"
+              minDate={startDate}
+              maxDate={new Date()}
+              selected={new Date()}
               selected={endDate}
-              onChange={setEndDate}
+              onChange={date => setEndDate(date)}
+              todayButton="Hoje"
+              renderDayContents={date => renderDayContents(date)}
+              disabledKeyboardNavigation
             />
           </div>
         </div>
